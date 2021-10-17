143267a608a18ba7773e93c7520ca43449a5b490
messin9 <messin9@gmail.comgit config --global user.name mesin9>
2021-10-17 14:04:45 +0300

    Initial Commit

---
+++ LoginTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginTestTask
+//
+//  Created by Алексей Мотин on 17.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
